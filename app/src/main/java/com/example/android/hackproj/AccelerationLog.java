e9d41d723743be16e81d2578c7233d468eec3ba6
danielli9988 <danielli9988@gmail.com>
2016-11-29 21:17:52 -0800

    arraylist works now

--- app/src/main/java/com/example/android/hackproj/AccelerationLog.java
+++ app/src/main/java/com/example/android/hackproj/AccelerationLog.java
@@ -2,29 +2,25 @@ package com.example.android.hackproj;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-
+import java.util.*;
 public class AccelerationLog extends AppCompatActivity {
     private RelativeLayout mLayout;
-    double times[]=new double[10];
+    private ListView listview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acceleration_log);
         mLayout = (RelativeLayout) findViewById(R.id.logLayout);
-        Bundle bundle = getIntent().getExtras();
-        times = bundle.getDoubleArray("speedLog");
-        for(double a:times){
-            accelerationLog(a);
-        }
-    }
-    public void accelerationLog(double time){
-        mLayout.addView(createNewTextView(Double.toString(time)));
-    }
-    private TextView createNewTextView(String text) {
-        final TextView textView = new TextView(this);
-        textView.setText(text);
-        return textView;
+        listview = (ListView) findViewById(R.id.speedList);
+        ArrayList<Double> listDouble = (ArrayList<Double>) getIntent().getSerializableExtra("arraylist");
+        ArrayAdapter<Double> arrayAdapter = new ArrayAdapter<Double>(
+                this,
+                android.R.layout.simple_list_item_1,
+                listDouble );
+        listview.setAdapter(arrayAdapter);
     }
 }

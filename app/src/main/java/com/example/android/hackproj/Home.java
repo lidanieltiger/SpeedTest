57388d04cffde1911cd94603707e506b6890a6cb
lidanieltiger <danielli9988@g.ucla.edu>
2016-12-15 16:54:39 -0800

    UI design

--- app/src/main/java/com/example/android/hackproj/Home.java
+++ app/src/main/java/com/example/android/hackproj/Home.java
@@ -15,6 +15,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.*;
 
@@ -25,20 +26,28 @@ public class Home extends AppCompatActivity {
     //date/time, arraylist of arrays (speedlog)
     SharedPreferences sharedpreferences;
     public static final String MyPREFERENCES = "ACCELERATION_DATA" ;
-
+    public String DAY_OF_YEAR;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE); //this contains my data
 
-        //TETING BELOW... TODO: remove this eventually
+        //TESTING BELOW... TODO: remove this eventually
         SharedPreferences preferences = getSharedPreferences(MyPREFERENCES, 0);
         SharedPreferences.Editor editor = preferences.edit();
         editor.clear();
         editor.commit();
         //CLEAR MYPREFERENCES FOR TESTING PURPOSES
 
+        //setting the number of logs recorded today on startup
+        Calendar c = Calendar.getInstance();
+        DAY_OF_YEAR= Integer.toString(c.get(Calendar.DAY_OF_YEAR));
+        TextView intro = (TextView)findViewById(R.id.introduction);
+        int numlogs = sharedpreferences.getInt("numLogsOn:"+DAY_OF_YEAR, 0); //number of logs in a given day
+        intro.setText("\t Welcome, Driver \n \n \t Sessions Recorded Today: "+numlogs);
+
+        //init toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -59,11 +68,17 @@ public class Home extends AppCompatActivity {
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
+        TextView intro = (TextView)findViewById(R.id.introduction);
+        SharedPreferences sharedpreferences = getSharedPreferences(Home.MyPREFERENCES, Context.MODE_PRIVATE);
         if (requestCode == 1) {
             if(resultCode == Activity.RESULT_OK){
+                //saving the logs to sharedpreferences via savelist
                 driveLog = (ArrayList<Double[]>) data.getSerializableExtra("result");
                 saveList(driveLog);
+
+                //changing the message for number of sessions recorded
+                int numlogs = sharedpreferences.getInt("numLogsOn:"+DAY_OF_YEAR, 0); //number of logs in a given day
+                intro.setText("\t Welcome, Driver \n \n \t Sessions Recorded Today: "+numlogs);
             }
             if (resultCode == Activity.RESULT_CANCELED) {
                 //nothing happens!
@@ -79,13 +94,10 @@ public class Home extends AppCompatActivity {
             //NOTE: a lot of the time there are duplicate points that are being overriden here. need to tweak my algorithm so this happens
             //LESS OFTEN...
         }
-        Calendar c = Calendar.getInstance();
-        String day = Integer.toString(c.get(Calendar.DAY_OF_YEAR));
-        int numlogs = sharedpreferences.getInt("numLogsOn:"+day, 0);
-        Log.d("homelog", "numlogs: "+numlogs);
-        String logdate = day+"index:"+Integer.toString(numlogs); //(day)index:0,1,2...
+        int numlogs = sharedpreferences.getInt("numLogsOn:"+DAY_OF_YEAR, 0);
+        String logdate = DAY_OF_YEAR+"index:"+Integer.toString(numlogs); //(day)index:0,1,2...
         editor.putStringSet(logdate, set);
-        editor.putInt("numLogsOn:"+day, ++numlogs);
+        editor.putInt("numLogsOn:"+DAY_OF_YEAR, ++numlogs);
         editor.commit();
     }
     @Override
